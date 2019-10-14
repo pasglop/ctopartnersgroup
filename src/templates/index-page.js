37fd9d0ec02765994b3e132a2ac237b15ae67730
pasglop <david.delpuech@gmail.com>
2019-10-14 10:46:21 +0200

    Format HP

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -11,56 +11,28 @@ export const IndexPageTemplate = ({
   herosubheading,
   mainpitch
 }) => (
-  <div>hero:{heroimg}
-    <div
-      className="full-width-image margin-top-0"
+  <div>
+    <section
+      className="hero is-primary is-fullheight-with-navbar"
       style={{
         backgroundImage: `url(${
-            !!heroimg && !!heroimg.childImageSharp ? heroimg.childImageSharp.fluid.src : heroimg
+            !!heroimg.childImageSharp ? heroimg.childImageSharp.fluid.src : heroimg
         })`,
         backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(46,163,242)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {heroheading}
-        </h1>
-        <h2
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(46,163,242)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {herosubheading}
-        </h2>
+      <div className="hero-body ">
+          <div className="container">
+              <h1 className="title">
+                  {heroheading}
+              </h1>
+              <h2 className="subtitle">
+                  {herosubheading}
+              </h2>
+          </div>
       </div>
-    </div>
-    <section className="section section--gradient" style={{ backgroundColor: 'rgb(46,163,242)' }}>
+    </section>
+    <section className="section section-gradient" style={{ backgroundColor: 'rgb(46,163,242)' }}>
       <div className="container" >
         <div className="section">
           <div className="columns">
@@ -118,14 +90,14 @@ IndexPageTemplate.propTypes = {
 };
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.heroimg}
-        heading={frontmatter.heroheading}
-        subheading={frontmatter.herosubheading}
+          heroimg={frontmatter.heroimg}
+          heroheading={frontmatter.heroheading}
+          herosubheading={frontmatter.herosubheading}
         mainpitch={frontmatter.mainpitch}
       />
     </Layout>
