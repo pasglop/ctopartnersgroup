7b0e4b4851b12ba72b9ec18091dd442760ff1916
pasglop <david.delpuech@gmail.com>
2019-10-16 09:49:54 +0200

    Pb build sur npm

--- src/templates/offer-page.js
+++ src/templates/offer-page.js
@@ -4,7 +4,6 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Features from '../components/Features';
 import Testimonials from '../components/Testimonials';
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
 import GatsbyImage from "gatsby-image";
 import BackgroundImage from 'gatsby-background-image';
 
